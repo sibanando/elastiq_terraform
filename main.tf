9513c21465849f5c8463e1371c16a602dfcf9565
sibanando <30410459+sibanando@users.noreply.github.com>
2024-10-31 15:38:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,15 +90,6 @@ module "ec2_instance" {
 #  bucket = "testayush" # change this
 #}
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
 
