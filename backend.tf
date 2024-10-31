2e599154141e2307c6c10c9996cd6b557c5ba543
sibanando <30410459+sibanando@users.noreply.github.com>
2024-10-31 15:38:37 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "testayush2"
-    key            = "jenkins/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
